4df9a690adc9697fc34fba1bf2c98b4b7749da95
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-09-26 17:28:59 +0530

    len of ll

---
+++ linkedlist/singly ll/intgetlen.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    struct Node* next;
+};
+
+typedef struct Node node;
+
+void insert(node** head, int val) {
+    node* curr = new node();
+    curr->val = val;
+    curr->next = nullptr;  // Set the new node's next to nullptr
+
+    if (*head == nullptr) {
+        // If the list is empty, make the new node the head
+        *head = curr;
+    } else {
+        // Find the last node and update its next pointer
+        node* last = *head;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = curr;
+    }
+}
+
+
+void display(node* head) {
+    node* curr = head;
+    if (head == NULL) {
+        cout << "underflow" << endl;
+    } else {
+        while (curr != NULL) {
+            cout << curr->val;
+            if (curr->next) {
+                cout << "->";
+            }
+            curr = curr->next;
+        }
+    }
+}
+
+int getlen(node* head) {
+    int cnt = 0;
+    while (head) {
+        cnt++;
+        head = head->next;
+    }
+    return cnt;
+}
+
+int main() {
+    node* head = NULL;
+    insert(&head, 1);
+    insert(&head, 2);
+    insert(&head, 3);
+    display(head);
+    cout<<"\nlength of linked list:"<<getlen(head)<<endl;
+    return 0;
+}
