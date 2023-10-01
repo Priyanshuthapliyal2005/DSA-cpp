94f98d2e15504830d6e0cef9605f2667846cffcd
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-10-01 23:44:27 +0530

    recurssion

---
+++ recursion/sumofn.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void sumN(int i, int sum) {
+    if (i == 0) {
+        cout << "Sum of first 'N' numbers: " << sum;
+        return;
+    }
+    sumN(i - 1, sum + i);
+}
+
+int main() {
+    int n;
+    cout << "Enter first N numbers:";
+    cin >> n;
+    sumN(n, 0);
+    return 0;
+}
