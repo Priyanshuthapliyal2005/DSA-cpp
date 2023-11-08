ca598c2b37230eb21b0b903d77ceea13d7fea68c
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-11-08 22:12:45 +0530

    traversal

--- tree/traversal in tree/preorder.cc
+++ tree/traversal in tree/preorder.cc
@@ -14,37 +14,71 @@ public:
     }
 };
 
-node *buildtree(node* root)
+node *buildtree()
 {
     cout << "Enter the data:" << endl;
     int data;
     cin >> data;
-    root = new node(data);
+    node *root = new node(data);
     if (data == -1)
     {
         return nullptr;
     }
     cout << "Enter data for inserting in left of " << data << endl;
-    root->left = buildtree(root->left);
+    root->left = buildtree();
     cout << "Enter data for inserting in right of " << data << endl;
-    root->right = buildtree(root->right);
+    root->right = buildtree();
     return root;
 }
 
-void preorder(node* root){
-    if(root==NULL){
+void iterativePreorder(node *root)
+{
+    if (root == nullptr)
+    {
         return;
     }
-    cout<<root->data<<" ";
+
+    stack<node *> s;
+    s.push(root);
+
+    while (!s.empty())
+    {
+        node *current = s.top();
+        s.pop();
+
+        cout << current->data << " ";
+
+        if (current->right != nullptr)
+        {
+            s.push(current->right);
+        }
+        if (current->left != nullptr)
+        {
+            s.push(current->left);
+        }
+    }
+}
+
+
+void preorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
 }
 
 int main()
 {
-    node* root = nullptr;
-    root = buildtree(root);
-    cout<<"preorder traversal result:"<<endl;
+    node *root = buildtree();
+    cout << "Iterative Preorder traversal result:" << endl;
+    iterativePreorder(root);
+
+    cout << "\nRecursive Preorder traversal result:" << endl;
     preorder(root);
+
     return 0;
 }
